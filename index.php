f4d78e7d41cd4adf03295406bbacb1eabfcb4d95
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-02-17 22:33:24 +0100

    end

--- index.php
+++ index.php
@@ -4,19 +4,3 @@ require_once __DIR__ . '/classes/Product.php';
 require_once __DIR__ . '/classes/User.php';
 require_once __DIR__ . '/classes/Shirt.php';
 
-$user = new User('Chiara', 'Passaro', 'chiara@email.it', '5432300');
-$creditCard = new CreditCard('Chiara Passaro', '65439008764333', '04/23', 532);
-// try {
-//   $user->setPaymentMethod(new CreditCard('Chiara Passaro', '65439008764333', '04/23', 532));
-// } catch (TypeError $error) {
-//   echo $error->getMessage();
-// }
-
-try {
-    $user->setPaymentMethod($creditCard);
-} catch (TypeError $error) {
-    echo $error->getMessage();
-}
-
-
-var_dump($user->getPaymentMethod());
